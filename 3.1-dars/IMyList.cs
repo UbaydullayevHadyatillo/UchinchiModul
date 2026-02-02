2dcca8cbfc7b9797e0195d7d07ca36c99eaf888e
UbaydullayevHadyatillo <ubaydullayevhp@gmail.com>
2026-02-02 11:09:49 +0500

    add

---
+++ 3.1-dars/IMyList.cs
@@ -0,0 +1,6 @@
+﻿namespace _3._1_dars
+{
+    public interface IMyList
+    {
+    }
+}
---
+++ 3.1-dars/MyList.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._1_dars;
+
+public class MyList : IMyList
+{
+
+    private int _capacity;
+
+    public int Capacity
+    {
+        get { return _capacity; }
+        set { _capacity = value; }
+    }
+
+    public MyList(int capacity = 4)
+    {
+        
+    }
+    bool IMyList.Add(int num)
+    {
+        throw new NotImplementedException();
+    }
+
+    bool IMyList.Contains(int num)
+    {
+        throw new NotImplementedException();
+    }
+
+    int IMyList.GetById(int index)
+    {
+        throw new NotImplementedException();
+    }
+
+    bool IMyList.IndexOf(int num)
+    {
+        throw new NotImplementedException();
+    }
+
+    bool IMyList.RemovAll(int num)
+    {
+        throw new NotImplementedException();
+    }
+
+    bool IMyList.RemovAt(int index)
+    {
+        throw new NotImplementedException();
+    }
+
+    bool IMyList.Remove(int num)
+    {
+        throw new NotImplementedException();
+    }
+}
